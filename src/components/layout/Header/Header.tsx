8ca8ed1140c4d9eb5d1680614f7ebcfde0c9b8d9
Caio Souza <caioss20.cs@gmail.com>
2026-06-15 15:22:49 -0300

    feat: new header component

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,14 +1,16 @@
 import { Container } from "../Container";
+import torraLogo from '../../../assets/logos/torraLogo.svg'
 
 
 export function Header() {
     return (
         <header className="bg-[var(--color-dark)] text-[var(--color-white)]">
-            <Container>
-                <div className="flex items-center justify-between">
+            <Container className="flex h-20 items-center">
+                <div className="flex w-full items-center justify-between">
                     <div className="flex items-center">
-                        <img src="/torraLogo.svg" alt="Lojas Torra" />
-                        <h2>Torra <br /> Cartão</h2>
+                        <img className="w-28" src={torraLogo} alt="Cartão Torra" />
+
+                        <p>Torra <br /> Cartão</p>
                     </div>
 
                     <nav className="flex items-center gap-6">
